1fcbb858046cf79f2d7b151f771ec9d06f8d93b5
neeleshmishra <7879neelesh@gmail.com>
2026-07-30 18:45:30 +0530

    Add Spring Boot backend initial project files

---
+++ knowledge-platform/knowledge-platform/src/test/java/com/knowledge/knowledge_platform/KnowledgePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledge.knowledge_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
