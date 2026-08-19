1f459ba978f1acd1e6051942ad58af2afbed6072
Naga-Prashanthi <239x1a3329@gmail.com>
2026-08-19 17:52:13 +0530

    Remove project code from main

--- backend/src/main/java/com/infosys/knowledgeplatform/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infosys.knowledgeplatform.repository;
-
-import com.infosys.knowledgeplatform.model.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- backend/src/main/java/com/infosys/knowledgeplatform/repository/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infosys.knowledgeplatform.repository;
-
-import com.infosys.knowledgeplatform.model.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
