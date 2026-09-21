3c5e21ef2f1427bd0da9a59326bf39a725542e9b
gomathiselvi-g <gomathikkn10a@gmail.com>
2026-09-21 18:36:14 +0530

    Access controll

--- OKGIP/backend/src/main/java/com/okip/security/config/SecurityConfig.java
+++ OKGIP/backend/src/main/java/com/okip/security/config/SecurityConfig.java
@@ -43,43 +43,40 @@ public class SecurityConfig {
     // CORS CONFIGURATION
     // =========================================================
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(
-                List.of(
-                        "https://okgip-frontend-nxes.onrender.com"
-                ));
-
-        configuration.setAllowedMethods(
-                List.of(
-                        "GET",
-                        "POST",
-                        "PUT",
-                        "PATCH",
-                        "DELETE",
-                        "OPTIONS"
-                ));
-
-        configuration.setAllowedHeaders(
-        List.of("*")
-);
-
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 
     // =========================================================
     // SECURITY FILTER CHAIN
     // =========================================================
+     @Bean
+CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOriginPatterns(
+            List.of("https://okgip-frontend-nxes.onrender.com")
+    );
+
+    configuration.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+    );
+
+    configuration.setAllowedHeaders(
+            List.of("*")
+    );
+
+    configuration.setExposedHeaders(
+            List.of("Authorization", "Content-Type")
+    );
+
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+}
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)
